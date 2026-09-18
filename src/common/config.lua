48839b373117fc7faf474a842cb4331c95e7cca7
nexox9official-source <nexox9official@gmail.com>
2026-09-18 19:26:18 +0200

    Add shared AstralNet configuration

---
+++ src/common/config.lua
@@ -0,0 +1,20 @@
+return {
+  NAME = "Computer Link",
+  NETWORK_NAME = "AstralNet",
+  VERSION = "0.1.0",
+  PROTOCOL_VERSION = 1,
+
+  MAGIC = "COMPUTER_LINK",
+  PROTOCOL = "astralnet.mer.v1",
+  SERVER_HOSTNAME = "MER",
+
+  ROOT = "/computer-link",
+  DATA_DIR = "/computer-link/data",
+  DATABASE_FILE = "/computer-link/data/mer.db",
+
+  USERNAME_MIN = 3,
+  USERNAME_MAX = 20,
+  MESSAGE_MAX = 500,
+
+  GITHUB_RAW = "https://raw.githubusercontent.com/nexox9official-source/computer-link/main/"
+}
