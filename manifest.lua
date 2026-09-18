65adab0c8a143cc656ec70530a4e6e4f131db071
nexox9official-source <nexox9official@gmail.com>
2026-09-18 19:26:14 +0200

    Add Computer Link manifest

---
+++ manifest.lua
@@ -0,0 +1,16 @@
+return {
+  name = "Computer Link",
+  version = "0.1.0",
+  protocol_version = 1,
+  files = {
+    "manifest.lua",
+    "boot.lua",
+    "update.lua",
+    "src/common/config.lua",
+    "src/common/util.lua",
+    "src/common/network.lua",
+    "src/server/database.lua",
+    "src/server/server.lua",
+    "src/client/client.lua"
+  }
+}
