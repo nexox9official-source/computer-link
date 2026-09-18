5c5ff3a084bcf5c2a3cfd2e1e044be186470c23d
nexox9official-source <nexox9official@gmail.com>
2026-09-18 20:03:09 +0200

    Add immutable server Computer Link policy

---
+++ server-datapack/data/computercraft/lua/rom/modules/main/computer_link_policy.lua
@@ -0,0 +1,11 @@
+-- Computer Link server policy.
+-- This file lives in CraftOS ROM through the server datapack, so normal
+-- ComputerCraft users cannot edit it from their computer filesystem.
+return {
+  version = 1,
+
+  github_raw = "https://raw.githubusercontent.com/nexox9official-source/computer-link/main/",
+  hack_operator_ids = {
+    [1] = true
+  }
+}
